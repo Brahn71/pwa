e74b0541ddbf8c51e680d4e185047f7aa05550d8
Brahn71 <mraudales71@gmail.com>
2026-03-05 18:35:17 -0600

    Fix sw path

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 if ('serviceWorker' in navigator) {
   console.log('Service Worker es compatible');
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
+    navigator.serviceWorker.register('./sw.js')
       .then(reg => console.log('Service Worker registrado con éxito (scope):', reg.scope))
       .catch(err => console.log('Fallo al registrar el Service Worker:', err));
   });
